0f9831e2ad3fdd19fcc2e736f45f1da3fda27cfb
jakob.seeberger@student.htl-rankweil.at <jakob.seeberger@student.htl-rankweil.at>
2026-03-04 10:18:47 +0000

    Edited MainLayout.Dashboards and MainLayout

--- client_code/MainLayout/__init__.py
+++ client_code/MainLayout/__init__.py
@@ -1,27 +1,14 @@
 from ._anvil_designer import MainLayoutTemplate
 from anvil import *
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
-import anvil.server
-
-from .PageTables import PageTables
-from .PageDashboards import PageDashboards
 
 class MainLayout(MainLayoutTemplate):
   def __init__(self, **properties):
     self.init_components(**properties)
-    self.show_page(PageTables())
-    def show_page(self, page):
-      self.content_panel.clear()
-    self.content_panel.add_component(page)
-
-
 
   @handle("btn_infos", "click")
   def btn_infos_click(self, **event_args):
-      self.show_page(PageTables())
-   
+    open_form("Pages.Table")        # <-- anpassen an deinen Pfad!
+
   @handle("btn_stats", "click")
   def btn_stats_click(self, **event_args):
-    self.show_page(PageDashboards())
+    open_form("Pages.Dashboards")   # <-- anpassen an deinen Pfad!
